abd644241014109a887a309c271a4de14be9f265
Yuta Uchida <yuchida4dev@gmail.com>
2025-02-22 11:10:21 +0900

    add horizontal photo gallery

--- metal-mobile-demo/Pages/PlaygroundView.swift
+++ metal-mobile-demo/Pages/PlaygroundView.swift
@@ -10,10 +10,36 @@ import SwiftUI
 
 struct PlaygroundView: View {
     @State var selectedImages: [PhotosPickerItem] = []
+    @State var images: [UIImage] = []
 
     var body: some View {
-        PhotosPicker(selection: $selectedImages, matching: .images){
-            Text("Pick Image")
+        VStack{
+            PhotosPicker(selection: $selectedImages, matching: .images){
+                Text("Pick Image")
+            }
+
+            ScrollView(.horizontal){
+                HStack{
+                    ForEach(images, id: \.self){ image in
+                        Image(uiImage: image)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 250, height: 300)
+                    }
+                }
+            }
+            
+        }
+        .onChange(of: selectedImages){
+            Task {
+                images = []
+                for item in selectedImages {
+                    guard let data = try await item.loadTransferable(type: Data.self) else {return}
+                    guard let uiImage = UIImage(data: data) else { return }
+                    images.append(uiImage)
+                }
+                
+            }
         }
     }
 }

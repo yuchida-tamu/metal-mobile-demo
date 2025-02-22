2cfd98d395363ce7470cef4f9586f0a1373273fd
Yuta Uchida <yuchida4dev@gmail.com>
2025-02-22 10:47:24 +0900

    init

---
+++ metal-mobile-demo/Pages/PlaygroundView.swift
@@ -0,0 +1,18 @@
+//
+//  PlaygroundView.swift
+//  metal-mobile-demo
+//
+//  Created by Yuta Uchida on 2025/02/22.
+//
+
+import SwiftUI
+
+struct PlaygroundView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PlaygroundView()
+}

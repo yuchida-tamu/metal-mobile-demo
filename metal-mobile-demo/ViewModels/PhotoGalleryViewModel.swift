629891b9000405cd1927f1980ede23e83d33c441
Yuta Uchida <yuchida4dev@gmail.com>
2025-02-23 22:22:12 +0900

    remove unnecessary permission

---
+++ metal-mobile-demo/ViewModels/PhotoGalleryViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  PhotoGalleryViewModel.swift
+//  metal-mobile-demo
+//
+//  Created by Yuta Uchida on 2025/02/23.
+//
+
+import SwiftUI
+
+struct PhotoGalleryViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PhotoGalleryViewModel()
+}
